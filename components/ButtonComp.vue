05742fcc5436f86e6a99bf9d3ea3e8a5f6f2754f
superboo0311 <60676245+superboo0311@users.noreply.github.com>
2023-05-03 22:47:25 -0400

    fix style :

--- components/ButtonComp.vue
+++ components/ButtonComp.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex h-12 w-fit items-center justify-center rounded-lg bg-red-950 p-5 text-white"
+    class="flex h-12 w-fit items-center justify-center rounded-lg bg-red-950 p-5 text-white hover:bg-black"
   >
     <span>Button</span>
   </div>
--- components/CardComp.vue
+++ components/CardComp.vue
@@ -4,9 +4,12 @@
       class="max-h-full max-w-full"
       src="https://www.mcdonalds.com/content/dam/sites/usa/nfl/publication/1pub_BigMacSauceCup_2336x1040.jpg"
     />
-    <div class="flex flex-grow flex-col text-center">
-      <span>Title Here</span><span>Get new Big Mac Now</span>
-      <div class="flex-grow"><ButtonComp /></div>
+    <div class="flex flex-grow flex-col pl-5 pr-5 text-center">
+      <span class="p-4">Title Here</span>
+      <span>Get new Big Mac Now</span>
+      <div class="flex flex-grow items-center justify-center">
+        <ButtonComp />
+      </div>
     </div>
   </div>
 </template>
